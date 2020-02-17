0378cd911a5c9d6702c28aa76e1223b8f490bc25
cristian-rincon <cristian.o.rincon.b>
2020-02-17 09:03:34 -0500

    newspaper receipe fixed

--- web_scraper/newspaper_receipe.py
+++ web_scraper/newspaper_receipe.py
@@ -1,38 +1,40 @@
-import argparse
-import logging
 import pandas as pd
 from urllib.parse import urlparse
-
+import argparse
+import logging
 logging.basicConfig(level=logging.INFO)
+
+
 logger = logging.getLogger(__name__)
 
 
 def main(filename):
-    logger.info('Starting cleaning process...')
+    logger.info('Starting cleaning process')
 
     df = _read_data(filename)
     newspaper_uid = _extract_newspaper_uid(filename)
     df = _add_newspaper_uid_column(df, newspaper_uid)
     df = _extract_host(df)
 
+    return df
+
 
 def _read_data(filename):
-    logger.info(f'Reading file {filename}...')
+    logger.info('Reading file {}'.format(filename))
 
     return pd.read_csv(filename)
 
 
 def _extract_newspaper_uid(filename):
-    logger.info('Extracting newspaper uid...')
+    logger.info('Extracting newspaper uid')
     newspaper_uid = filename.split('_')[0]
 
-    logger.info(f'Newspaper uid detected: {newspaper_uid}...')
-
+    logger.info('Newspaper uid detected: {}'.format(newspaper_uid))
     return newspaper_uid
 
 
 def _add_newspaper_uid_column(df, newspaper_uid):
-    logger.info(f'Filling newspaper_uid column with {newspaper_uid}')
+    logger.info('Filling newspaper_uid column with {}'.format(newspaper_uid))
     df['newspaper_uid'] = newspaper_uid
 
     return df
@@ -45,12 +47,13 @@ def _extract_host(df):
     return df
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('filename',
-                        help='Path to dirty data',
+                        help='The path to the dirty data',
                         type=str)
+
     args = parser.parse_args()
+
     df = main(args.filename)
     print(df)
